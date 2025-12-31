0ce5f5a3b6f157825fbc8c266c6ba5d9550798d4
litaliano00-dev <litaliano00.contact@gmail.com>
2025-12-31 17:38:31 +0100

    Add terminal.js for WebSocket terminal functionality

---
+++ frontend/terminal.js
@@ -0,0 +1,28 @@
+const term = new Terminal({
+  cursorBlink: true,
+  fontSize: 14,
+  theme: {
+    background: "#000000"
+  }
+});
+
+term.open(document.getElementById("terminal"));
+term.write("Connecting to Arch Linux...\r\n");
+
+const socket = new WebSocket(ws://${location.host});
+
+socket.onmessage = (event) => {
+  term.write(event.data);
+};
+
+term.onData(data => {
+  socket.send(data);
+});
+
+socket.onopen = () => {
+  term.write("\r\nConnected.\r\n");
+};
+
+socket.onclose = () => {
+  term.write("\r\nDisconnected.\r\n");
+};
