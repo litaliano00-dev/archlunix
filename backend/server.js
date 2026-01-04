e2a632ab7a4634050db3ca95067967bf6a7086d4
litaliano00-dev <litaliano00.contact@gmail.com>
2026-01-04 17:38:42 +0100

    Refactor server.js for cleaner code structure

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,8 @@ import pty from "node-pty";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename =
-  fileURLToPath(import.meta.url);
-const __dirname =
-  path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
@@ -23,21 +21,21 @@ const wss = new WebSocketServer({ server });
 wss.on("connection", (ws) => {
   console.log("🟢 Client connected");
 
-       const shell = pty.spawn("docker", [
-         "run",
-         "--rm",
-         "-i",
-         "--hostname=archlunix",
-         "archlunix"
-        ], {
-          name: "xterm-256color",
-          cols: 80,
-          rows: 24,
-          env: process.env
-        });
+  const shell = pty.spawn("docker", [
+    "run",
+    "--rm",
+    "-i",
+    "--hostname=archlinux",
+    "archlunix"
+  ], {
+    name: "xterm-256color",
+    cols: 80,
+    rows: 24,
+    env: process.env
+  });
 
-       shell.on("data", data => ws.send(data));
+  shell.on("data", data => ws.send(data));
   ws.on("message", msg => shell.write(msg));
 
-       ws.on("close", () => shell.kill());
+  ws.on("close", () => shell.kill());
 });
